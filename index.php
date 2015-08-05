ebf669c9a7e26028238edd7800ecf785418b1bca
faith <whh306318848@126.com>
2015-08-05 18:23:31 +0800

    更新了示例程序
    更新了示例程序

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
 
-require_once (dirname( __FILE__ ).'/Department.php');
-require_once (dirname( __FILE__ ).'/CreateConnection.php');
-require_once (dirname( __FILE__ ).'/User.php');
-require_once (dirname( __FILE__ ).'/Tag.php');
+require_once (dirname(__FILE__) . '/Department.php');
+require_once (dirname(__FILE__) . '/CreateConnection.php');
+require_once (dirname(__FILE__) . '/User.php');
+require_once (dirname(__FILE__) . '/Tag.php');
+require_once (dirname(__FILE__) . '/Material.php');
+require_once (dirname(__FILE__) . '/SendMessage.php');
 
-$corpid = "Your weixin's corpid";
-$corpsecret = "Your weixin's corpsecret";
+$corpid = "your corpid";
+$corpsecret = "your corpsecret";
 
 $createConnection = new CreateConnection();
-$token = $createConnection->getAccessToken($corpid, $corpsecret);
+$token = $createConnection -> getAccessToken($corpid, $corpsecret);
 
 if ($token) {
-	echo "Token:".$token."<br/>";
+	echo "Token:" . $token . "<br/>";
 	echo "<pre>";
 	//$department = new Department($token);
 	//var_dump($department->getDepartmentList());
@@ -22,7 +24,7 @@ if ($token) {
 	//var_dump($department->deleteDepartment(13));
 	//var_dump($department->getDepartmentByID(3));
 	//var_dump($department->getDepartmentsByName('生'));
-	
+
 	//$user = new User($token);
 	//var_dump($user->getUserList(1, 1));
 	//var_dump($user->getUserListDetails(1, 1));
@@ -33,8 +35,8 @@ if ($token) {
 	//var_dump($user->batchDeleteUsers(array('ewewew3', 'ewewew4')));
 	//var_dump($user->getUserByID('xuyuanping'));
 	//var_dump($user->getUserByName('吴昊骅'));
-	
-	$tag = new Tag($token);
+
+	//$tag = new Tag($token);
 	//var_dump($tag->getTagList());
 	//var_dump($tag->getTagByID(1));
 	//var_dump($tag->getTagByName("测试", FALSE));
@@ -44,10 +46,25 @@ if ($token) {
 	//var_dump($tag->createTag('测试3', 3));
 	//var_dump($tag->updateTag(3, '测试5'));
 	//var_dump($tag->deleteTag(3));
+
+	//$material = new Material($token);
+	//var_dump($material->uploadTemporaryMaterial(Material::MEDIA_TYPE_IMAGE, dirname( __FILE__ )."/b.png"));
+	//var_dump($material->getTemporaryMaterial("1I_FViSA9hArOZWzKJ0idhTaGwjbYARNscDwfsESs-wfpeDgOCMAj7Av2VuUvTnt7T5aSf-XoaSlOVDi-_s2lFw"));
+	//var_dump($material->uploadPermanentMaterial(4, Material::MEDIA_TYPE_IMAGE, dirname( __FILE__ )."/b.png"));
+	//var_dump($material->getPermanentMaterial("2_AL2HYxmlBDgDV9UT8uoMWJOC-7hzIKOUZQ8Thcof-Eg8wxT6TkOpnxs1VXOJmqJw_G8xI4QZrJs1L-EB0khsQ", 4));
+	//var_dump($material->deletePermanentMaterial("2_AL2HYxmlBDgDV9UT8uoMWJOC-7hzIKOUZQ8Thcof-Eg8wxT6TkOpnxs1VXOJmqJw_G8xI4QZrJs1L-EB0khsQ", 4));
+	//var_dump($material->getMaterialCount(4));
+	//var_dump($material->getMaterialList(Material::MEDIA_TYPE_VOICE, 4, 0, 50));
 	
+
+	//$sendMessage = new SendMessage($token);
+	//var_dump($sendMessage->sendFile(4, "1KQuH3LMcYGIj3t0nM-i1wyZMo-TpULxFnkEoYpB2r15Th6P86R5EUnzATFSK_Uq0rB1yPX8IhwOglcqSbhUSgA"));
+	//var_dump($sendMessage->sendText(4, "Holiday Request For Pony(\"http://xxxxx\")"));
+	//var_dump($sendMessage->sendImage(4, "271UhK8-pLZ1gy1G5z4ccTUJKPrFn7iFD2GsPk8mt6d5vJTYVANmxHSzF1wkM_qB_C3ggzMKcZhZl-Nmpx0e8QQ"));
+	//var_dump($sendMessage->sendVoice(4, "2pAg95-1xk2v-GK7veGVhz6Zoe8qZj20bu8ixRV6M9j_mdgrYMF16p0DTdSCOuASo1gjmOcgafj3Gd4LAEfzciw"));
+
 	echo "</pre>";
-}else {
+} else {
 	echo "Token 获取失败";
 }
-
 ?>
