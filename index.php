ad46ac3f719862bf95389fc2d0707498ed5c8a64
faith <whh306318848@126.com>
2015-08-06 00:02:53 +0800

    完善例程
    增加了对发送消息底层类库的调用语句

--- index.php
+++ index.php
@@ -33,8 +33,8 @@ if ($token) {
 	//var_dump($user->updateUser('ewewew', '测试人员', 1, '程序媛', '19009090808', 2, 'dadadas@112.com', FALSE, 2, FALSE, array('外号'=>'二货')));
 	//var_dump($user->deleteUser('ewewew'));
 	//var_dump($user->batchDeleteUsers(array('ewewew3', 'ewewew4')));
-	//var_dump($user->getUserByID('xuyuanping'));
-	//var_dump($user->getUserByName('吴昊骅'));
+	//var_dump($user->getUserByID('xxx'));
+	//var_dump($user->getUserByName('faith'));
 
 	//$tag = new Tag($token);
 	//var_dump($tag->getTagList());
@@ -48,13 +48,13 @@ if ($token) {
 	//var_dump($tag->deleteTag(3));
 
 	//$material = new Material($token);
-	//var_dump($material->uploadTemporaryMaterial(Material::MEDIA_TYPE_IMAGE, dirname( __FILE__ )."/b.png"));
+	//var_dump($material->uploadTemporaryMaterial(Material::MEDIA_TYPE_VIDEO, dirname( __FILE__ )."/c.mp4"));
 	//var_dump($material->getTemporaryMaterial("1I_FViSA9hArOZWzKJ0idhTaGwjbYARNscDwfsESs-wfpeDgOCMAj7Av2VuUvTnt7T5aSf-XoaSlOVDi-_s2lFw"));
 	//var_dump($material->uploadPermanentMaterial(4, Material::MEDIA_TYPE_IMAGE, dirname( __FILE__ )."/b.png"));
 	//var_dump($material->getPermanentMaterial("2_AL2HYxmlBDgDV9UT8uoMWJOC-7hzIKOUZQ8Thcof-Eg8wxT6TkOpnxs1VXOJmqJw_G8xI4QZrJs1L-EB0khsQ", 4));
 	//var_dump($material->deletePermanentMaterial("2_AL2HYxmlBDgDV9UT8uoMWJOC-7hzIKOUZQ8Thcof-Eg8wxT6TkOpnxs1VXOJmqJw_G8xI4QZrJs1L-EB0khsQ", 4));
 	//var_dump($material->getMaterialCount(4));
-	//var_dump($material->getMaterialList(Material::MEDIA_TYPE_VOICE, 4, 0, 50));
+	//var_dump($material->getMaterialList(Material::MEDIA_TYPE_IMAGE, 4, 0, 50));
 	
 
 	//$sendMessage = new SendMessage($token);
@@ -62,6 +62,10 @@ if ($token) {
 	//var_dump($sendMessage->sendText(4, "Holiday Request For Pony(\"http://xxxxx\")"));
 	//var_dump($sendMessage->sendImage(4, "271UhK8-pLZ1gy1G5z4ccTUJKPrFn7iFD2GsPk8mt6d5vJTYVANmxHSzF1wkM_qB_C3ggzMKcZhZl-Nmpx0e8QQ"));
 	//var_dump($sendMessage->sendVoice(4, "2pAg95-1xk2v-GK7veGVhz6Zoe8qZj20bu8ixRV6M9j_mdgrYMF16p0DTdSCOuASo1gjmOcgafj3Gd4LAEfzciw"));
+	//var_dump($sendMessage->sendVideo(0, "2wZarvJL7tZikb27ko-ov2Of7fVcnnIh5ZNJezgl5db-o7e7iWnI4zEvV9-s2xI7Fatc4LZw2pK32hiVNJ75GcA", "测试视频接口", "我就是测试一下视频发送接口"));
+	//var_dump($sendMessage->sendNews(4, array(array('title'=>'测试消息1', 'description'=>'这是测试消息1', 'url'=>'http://www.aiurbia.net', 'picurl'=>'http://www.aiurbia.net/images/yootheme/logo_green.png'), array('title'=>'测试消息2', 'description'=>'这是测试消息2', 'url'=>'http://www.youarebug.com', 'picurl'=>'http://www.youarebug.com/template/sin_lanker/sinkey/logo.png'))));
+	//var_dump($sendMessage->sendMpnewsByMediaID(4, "2N53V20kxM_83rjCKTsncW-0WhBLRQsPVxXhtElFyUGXJGSWk0FwKSw-IeV56srH3"));
+	//var_dump($sendMessage->sendMpnewsByContent(4, array(array('title'=>'测试消息1', 'thumb_media_id'=>'271UhK8-pLZ1gy1G5z4ccTUJKPrFn7iFD2GsPk8mt6d5vJTYVANmxHSzF1wkM_qB_C3ggzMKcZhZl-Nmpx0e8QQ', 'author'=>'faith', 'content_source_url'=>'http://www.youarebug.com', 'content'=>'测试消息1', 'digest'=>'测试信息，你信吗？', 'show_cover_pic'=>1), array('title'=>'测试消息2', 'thumb_media_id'=>'271UhK8-pLZ1gy1G5z4ccTUJKPrFn7iFD2GsPk8mt6d5vJTYVANmxHSzF1wkM_qB_C3ggzMKcZhZl-Nmpx0e8QQ', 'author'=>'faith', 'content_source_url'=>'http://www.aiurbia.net', 'content'=>'测试消息2', 'digest'=>'测试信息，你信吗？', 'show_cover_pic'=>1))));
 
 	echo "</pre>";
 } else {
