afc0961acb6aa1d993b2009281b6e454a77e8456
faith <whh306318848@126.com>
2015-08-06 18:45:46 +0800

    完善例程
    增加了对企业应用自定义菜单管理调用的示例代码

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once (dirname(__FILE__) . '/Tag.php');
 require_once (dirname(__FILE__) . '/Material.php');
 require_once (dirname(__FILE__) . '/SendMessage.php');
 require_once (dirname(__FILE__) . '/Agent.php');
+require_once (dirname(__FILE__) . '/Menu.php');
 
 $corpid = "your corpid";
 $corpsecret = "your corpsecret";
@@ -55,24 +56,88 @@ if ($token) {
 	//var_dump($material->getPermanentMaterial("2_AL2HYxmlBDgDV9UT8uoMWJOC-7hzIKOUZQ8Thcof-Eg8wxT6TkOpnxs1VXOJmqJw_G8xI4QZrJs1L-EB0khsQ", 4));
 	//var_dump($material->deletePermanentMaterial("2_AL2HYxmlBDgDV9UT8uoMWJOC-7hzIKOUZQ8Thcof-Eg8wxT6TkOpnxs1VXOJmqJw_G8xI4QZrJs1L-EB0khsQ", 4));
 	//var_dump($material->getMaterialCount(4));
-	//var_dump($material->getMaterialList(Material::MEDIA_TYPE_IMAGE, 4, 0, 50));
-	
+	//var_dump($material->getMaterialList(Material::MEDIA_TYPE_VIDEO, 4, 0, 50));
 
 	//$sendMessage = new SendMessage($token);
 	//var_dump($sendMessage->sendFile(4, "1KQuH3LMcYGIj3t0nM-i1wyZMo-TpULxFnkEoYpB2r15Th6P86R5EUnzATFSK_Uq0rB1yPX8IhwOglcqSbhUSgA"));
 	//var_dump($sendMessage->sendText(4, "Holiday Request For Pony(\"http://xxxxx\")"));
 	//var_dump($sendMessage->sendImage(4, "271UhK8-pLZ1gy1G5z4ccTUJKPrFn7iFD2GsPk8mt6d5vJTYVANmxHSzF1wkM_qB_C3ggzMKcZhZl-Nmpx0e8QQ"));
 	//var_dump($sendMessage->sendVoice(4, "2pAg95-1xk2v-GK7veGVhz6Zoe8qZj20bu8ixRV6M9j_mdgrYMF16p0DTdSCOuASo1gjmOcgafj3Gd4LAEfzciw"));
-	//var_dump($sendMessage->sendVideo(0, "2wZarvJL7tZikb27ko-ov2Of7fVcnnIh5ZNJezgl5db-o7e7iWnI4zEvV9-s2xI7Fatc4LZw2pK32hiVNJ75GcA", "测试视频接口", "我就是测试一下视频发送接口"));
-	//var_dump($sendMessage->sendNews(0, array(array('title'=>'我用爬虫一天时间“偷了”知乎一百万用户，只为证明PHP是世界上最好的语言', 'description'=>'来源： 爱编程', 'url'=>'http://www.w2bc.com/Article/54923'))));
+	//var_dump($sendMessage->sendVideo(4, "2hHlbQB5q0fr8z-hF8KyyPYiSZAMizYmeezRkpKxK4uu5fF0endeI81_2dfyRwBW4Y4oPP4fJqZTXPN3KCo9pwg", "测试视频接口", "我就是测试一下视频发送接口"));
+	//var_dump($sendMessage->sendNews(4, array(array('title'=>'我用爬虫一天时间“偷了”知乎一百万用户，只为证明PHP是世界上最好的语言', 'description'=>'来源： 爱编程', 'url'=>'http://www.w2bc.com/Article/54923'))));
 	//var_dump($sendMessage->sendMpnewsByMediaID(4, "2N53V20kxM_83rjCKTsncW-0WhBLRQsPVxXhtElFyUGXJGSWk0FwKSw-IeV56srH3"));
 	//var_dump($sendMessage->sendMpnewsByContent(4, array(array('title'=>'测试消息1', 'thumb_media_id'=>'271UhK8-pLZ1gy1G5z4ccTUJKPrFn7iFD2GsPk8mt6d5vJTYVANmxHSzF1wkM_qB_C3ggzMKcZhZl-Nmpx0e8QQ', 'author'=>'faith', 'content_source_url'=>'http://www.youarebug.com', 'content'=>'测试消息1', 'digest'=>'测试信息，你信吗？', 'show_cover_pic'=>1), array('title'=>'测试消息2', 'thumb_media_id'=>'271UhK8-pLZ1gy1G5z4ccTUJKPrFn7iFD2GsPk8mt6d5vJTYVANmxHSzF1wkM_qB_C3ggzMKcZhZl-Nmpx0e8QQ', 'author'=>'faith', 'content_source_url'=>'http://www.aiurbia.net', 'content'=>'测试消息2', 'digest'=>'测试信息，你信吗？', 'show_cover_pic'=>1))));
-	
+
 	//$agent = new Agent($token);
 	//var_dump($agent->getAgentByID(4));
 	//var_dump($agent->setAgentByID(4, "测试办公", "测试办公", FALSE, FALSE, 1, 1, 1));
 	//var_dump($agent->getAgentList());
 	//var_dump($agent->getAgentByName("测试办公", FALSE));
+
+	//$menu = new Menu($token);
+	/*
+	$button_list = array( 
+		array(
+			'name' => '点击1', 
+			'sub_button' => array( 
+				array(
+					'type' => 'click', 
+					'name' => '点击推事件', 
+					'key' => 'CLICK1001'
+				), 
+				array(
+					'type' => 'view', 
+					'name' => '跳转URL', 
+					'url' => 'http://www.aiurbia.net'
+				)
+			)
+		), 
+		array(
+			'name' => '扫码1', 
+			'sub_button' => array(
+				array(
+					'type'=>'scancode_push',
+					'name'=>'扫码推事件',
+					'key'=>'rselfmenu_0_1'
+				),
+				array(
+					'type'=>'scancode_waitmsg',
+					'name'=>'扫码带提示',
+					'key'=>'rselfmenu_0_0'
+				)
+			)
+		),
+		array(
+			'name' => '弹出1',
+			'sub_button' => array(
+				array(
+					'type'=>'pic_sysphoto',
+					'name'=>'系统拍照发图',
+					'key'=>'rselfmenu_1_0'
+				),
+				array(
+					'type'=>'pic_photo_or_album',
+					'name'=>'拍照或者相册发图',
+					'key'=>'rselfmenu_1_1'
+				),
+				array(
+					'type'=>'pic_weixin',
+					'name'=>'微信相册发图',
+					'key'=>'rselfmenu_1_2'
+				),
+				array(
+					'type'=>'location_select',
+					'name'=>'发送位置',
+					'key'=>'rselfmenu_2_0'
+				)
+			)
+		)
+	);
+	var_dump($menu -> createMenu(4, $button_list));
+	*/
+	//var_dump($menu->deleteMenu(4));
+	//var_dump($menu->getMenu(4));
+	
 
 	echo "</pre>";
 } else {
